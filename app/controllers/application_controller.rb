e8048aafc63101dfa5dfc2f2113e56144a77faa4
shisoshiso <66207602+shisoshiso@users.noreply.github.com>
2020-10-16 07:35:45 +0900

    ユーザー編集機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  # deviseのコントローラーから呼び出された場合は、configure_permitted_parametersが呼び出される
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
